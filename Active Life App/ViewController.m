dfd81edf71daf7370418208ff3f9e52a1b86d1f5
sdnmacmini10 <sdnmacmini10@sdnmacmini10s-Mac-mini-2.local>
2014-06-23 17:00:20 +0530

    Initial Commit

---
+++ Active Life App/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Active Life App
+//
+//  Created by sdnmacmini10 on 23/06/14.
+//  Copyright (c) 2014 sdnmacmini10. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
